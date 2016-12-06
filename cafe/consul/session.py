ec9fbfe06ddc37ba2dce8d5857640779ce653ae4
Arun Babu Neelicattu <arun.neelicattu@gmail.com>
2016-12-06 11:44:56 +1000

    session: improve session state handling
    - session create now returns boolean
    - introduce session life
    - invalidate sesson cleanly when destroying session
    - introduce session recreate method
    - recreate session if renew encounters NotFound exception

--- cafe/consul/session.py
+++ cafe/consul/session.py
@@ -1,4 +1,5 @@
 from os import getenv
+from time import time
 from twisted.internet import task, reactor, defer
 
 from cafe.logging import LoggedObject
@@ -24,6 +25,7 @@ class ConsulSessionWrapper(LoggedObject, ConsulBase.Session):
         if self.lock_delay is not None and 0 < self.lock_delay < 60:
             self.logger.debug('invalid lock-delay=%s specified, using defaults', self.lock_delay)
             self.lock_delay = 15
+        self._renewed_at = None
         self.heartbeat = task.LoopingCall(self.renew)
         reactor.callLater(0, self.create)
         reactor.addSystemEventTrigger('before', 'shutdown', self.destroy)
@@ -54,11 +56,14 @@ class ConsulSessionWrapper(LoggedObject, ConsulBase.Session):
 
         :param retry: retry later if creation fails
         :type retry: bool
+        :rtype: bool
         """
+        session_created = False
         try:
             self.logger.trace('attempting to create a new session')
             self._uuid = yield self.base.create(
                 self.name, behavior='delete', ttl=self.ttl, lock_delay=self.lock_delay)
+            self._renewed_at = time()
             self.logger.info('name=%s session=%s created', self.name, self.uuid)
             reactor.callLater(0, self.watch_for_session_change)
 
@@ -66,11 +71,31 @@ class ConsulSessionWrapper(LoggedObject, ConsulBase.Session):
                 reactor.callLater(0, self.heartbeat.start, interval=self.heartbeat_interval)
         except ConsulException as e:
             self.logger.warning(
-                'session=%s creation failed, retrying reason=%s',
-                self.uuid, e.message)
+                'name=%s, session=%s creation failed, retrying reason=%s', self.name, self.uuid, e.message)
             if retry:
                 # try again in SESSION_CREATE_RETRY_DELAY_SECONDS
                 reactor.callLater(self.SESSION_CREATE_RETRY_DELAY_SECONDS, self.create)
+        else:
+            session_created = True
+        defer.returnValue(session_created)
+
+    @defer.inlineCallbacks
+    def recreate(self, retry=True, force=False):
+        """
+        Recreate a session. If instance hsa a uuid and has remaining life, this is skipped unless forced.
+
+        :param retry: retry later if creation fails
+        :type retry: bool
+        :param force: force session recreation
+        :type force: bool
+        :rtype: bool
+        """
+        if self.uuid is not None and not force and self.life > 0:
+            self.logger.debug('name=%s session=%s session recreate requested but not forced, skipping as life is not 0')
+            defer.returnValue(False)
+        yield self.destroy()
+        created = yield self.create(retry=retry)
+        defer.returnValue(created)
 
     @defer.inlineCallbacks
     def watch_for_session_change(self, index=None):
@@ -96,7 +121,7 @@ class ConsulSessionWrapper(LoggedObject, ConsulBase.Session):
                 'indicates that the Consul server cluster is unhealthy. This client will attempt to create a new '
                 'session.'
             )
-            reactor.callLater(self.SESSION_CREATE_RETRY_DELAY_SECONDS, self.create)
+            reactor.callLater(0, self.recreate, force=True)
         else:
             # Since the session is valid, just go back to watching.
             reactor.callLater(0, self.watch_for_session_change, index=index)
@@ -108,32 +133,49 @@ class ConsulSessionWrapper(LoggedObject, ConsulBase.Session):
             if self.uuid is not None:
                 self.logger.trace('name=%s session=%s renewing session', self.name, self.uuid)
                 yield self.base.renew(self.uuid)
+                self._renewed_at = time()
         except NotFound:
-            self.logger.warning('Session %s renew failure: not found.', self.uuid)
+            self.logger.warning('name=%s session=%s renewal failed reason=session not found', self.name, self.uuid)
+            yield self.recreate(force=True)
         except ConsulException as e:
             self.logger.warning(
                 'session=%s renewal attempt failed reason=%s',
                 self.uuid, e.message
             )
 
+    @property
+    def life(self):
+        """
+        Life remaining for this session in seconds. Life is calculated as Session TTL - time since last renewal.
+
+        :rtype: float
+        """
+        if self._renewed_at is not None:
+            return max(self.ttl - (time() - self._renewed_at), 0.0)
+        return 0.0
+
     @defer.inlineCallbacks
     def destroy(self, dc=None):
         """Destroy a session if one is active, else do nothing."""
-        try:
-            if self.uuid is not None:
-                if self.heartbeat.running:
-                    self.logger.trace('name=%s session=%s stopping heartbeat', self.name, self.uuid)
-                    self.heartbeat.stop()
+        if self.uuid is None:
+            defer.returnValue(None)
 
-                self.logger.trace('name=%s session=%s destroying session', self.name, self.uuid)
-                yield self.base.destroy(self.uuid, dc=dc)
-                self.logger.info('name=%s session=%s destroyed session', self.name, self.uuid)
-                self._uuid = None
+        if self.heartbeat.running:
+            self.logger.trace('name=%s session=%s stopping heartbeat', self.name, self.uuid)
+            self.heartbeat.stop()
+
+        try:
+            self.logger.trace('name=%s session=%s destroying session', self.name, self.uuid)
+            yield self.base.destroy(self.uuid, dc=dc)
+            self.logger.info('name=%s session=%s destroyed session', self.name, self.uuid)
         except ConsulException as e:
             self.logger.warning(
-                'session=%s destruction attempt failed reason=%s',
-                self.uuid, e.message
+                'name=%s session=%s destruction attempt failed but session will be invalidated reason=%s',
+                self.name, self.uuid, e.message
             )
+        finally:
+            self._uuid = None
+            self._renewed_at = None
 
     @property
     def ready(self):
